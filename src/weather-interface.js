421bc4d064a5e029689dc16d88dde6bc3f2f7706
Craig Wann and David Ngo <craig@>
2018-08-06 16:50:16 -0700

    add weather info

---
+++ src/weather-interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=64381af634f8d8c39085031fc47a3b9d`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
+});
